6e7a120e763c295ef262c8cf22386fa49c794f70
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-11-06 12:27:34 -0300

    feat: configured database

--- src/main/java/com/rgp/game/rpg/Entity/User.java
+++ src/main/java/com/rgp/game/rpg/Entity/User.java
@@ -1,7 +1,7 @@
 package com.rgp.game.rpg.Entity;
 
 import jakarta.persistence.*;
-import jakarta.constraints.Email;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
